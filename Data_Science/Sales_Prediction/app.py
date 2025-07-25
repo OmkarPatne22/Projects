f07500a2048ad263582f5232e2d0fc22982adde7
OmkarPatne <omkar2252001@gmail.com>
2025-07-25 12:13:23 +0530

    update files

---
+++ Data_Science/Sales_Prediction/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+with open("model.pkl","rb") as p:
+    model = pickle.load(p)
+
+st.title("Sales Prediction Application")
+
+st.write ("Please Enter your Advertisement Invest in each to predict the sales.")
+
+TV = st.number_input("Advertising investment in TV", min_value = 0.0, format = "%.2f")
+RADIO = st.number_input("Advertising investment in Radio", min_value = 0.0, format = "%.2f")
+NEWSPAPER = st.number_input("Advertising investment in Newspaper", min_value = 0.0, format = "%.2f")
+
+if st.button("Predict Sales"):
+    features = np.array([[TV,RADIO,NEWSPAPER]])
+    predictions = model.predict(features)[0]
+    st.success(f"Predicted Sales: {predictions:.2f}")
+
+    
